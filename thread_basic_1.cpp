2c49ac6fb641148c095fa72f06c97af2bbe84d81
ashish <ashishrmodi16@gmail.com>
2015-08-01 00:57:11 -0700

    Added different function arguments for thread functions

--- thread_basic_1.cpp
+++ thread_basic_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <string>
 
 using namespace std;
 /*
@@ -7,14 +8,21 @@ using namespace std;
 2. with differernt type of the return argurments
 */
 
+class C
+{
+	int a;
+};
+
 // void funcitons
-int function_1()
+int function_1(int a, int b, string c, C e)
 {
 	std::cout << "Inside function 1"<<endl;
+	std :: cout << "a = " << a << " b = " << b << "String entered is " << c <<std::endl;
 	return 120;
 }
-int function_2()
+int function_2(string s)
 {
+	std :: cout << "String entererd is " << s << endl;
 	std::cout << "Inside function 2"<<endl;
 	return 130;
 }
@@ -22,8 +30,9 @@ int function_2()
 int main()
 {
 	// You have just created two objects and nothing more than is.
-	std:: thread  first(function_1);
-	std:: thread  second(function_2);
+	C a;
+	std:: thread  first(function_1, 10,20, "Thread first", a);
+	std:: thread  second(function_2, "Ashish Modi thread second" );
 	
 	// If you attempt to run the code with just two thread created with joining them, application shall crash at run time.
 	first.join();
