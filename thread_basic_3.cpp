c45abfb57b3e93d6ecf21ccabeb823ede3d3429f
ashish <ashishrmodi16@gmail.com>
2015-08-01 01:59:38 -0700

    thread::join() and thread::joinable() functions, difference illustrated

---
+++ thread_basic_3.cpp
@@ -0,0 +1,44 @@
+// Aim : To understand functionality of thread::join() and thread::joinable() functions.
+// What is difference between this two functions
+
+
+
+
+/*
+	thread::joinable()
+	- returns true or false value, 
+	- if thread is joinable, then returns true.
+	- else false
+
+*/
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void function_1()
+{
+	std::cout << "Function 1 " << std::endl;
+}
+
+int main()
+{
+	cout << "Hello World" << endl;
+	
+	std :: thread t1 (function_1);
+	
+	if(t1.joinable())
+	{
+		t1.join();
+	}
+	
+	if(t1.joinable())
+	{
+		cout << "T1 is still joinable" <<endl;
+	}
+	else
+	{
+		cout << "T1 is not joinable now" << endl;
+	}
+	
+}
