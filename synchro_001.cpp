b10b9b0ff1e4f5f3bc97bb829b75653cb4dd1396
ashish <ashishrmodi16@gmail.com>
2015-08-01 04:37:55 -0700

    Updated number of threads, and added observed output

--- synchro_001.cpp
+++ synchro_001.cpp
@@ -7,41 +7,52 @@
 #include <vector>
 using namespace std;
 
-struct Counter
-{
-	int value;
-	Counter() : value(0) {}
-	void increament()
-	{
-		value++;
-	}
+
+struct Counter {
+    int value;
+
+    Counter() : value(0){}
+
+    void increment(){
+        ++value;
+    }
 };
 
+int main(){
+    Counter counter;
 
-int main()
-{
-	cout << "Hello World" <<endl;
-	
-	Counter counter  ;
-	vector <thread> threads;
-	for(int i = 0 ; i < 5 ; i ++ )
-	{
-	
-	threads.push_back(  std::thread( [&counter]()
-	   {
-            for(int i = 0; i < 100; ++i)
-			{
-                counter.increament();
+    std::vector<std::thread> threads;
+    for(int i = 0; i < 250; ++i){
+        threads.push_back(std::thread([&counter](){
+            for(int i = 0; i < 100; ++i){
+                counter.increment();
             }
-       }));	
-	}
+        }));
+    }
 
-	for(auto& thread : threads)
-	{
-		// Thread Join for each thread.
+    for(auto& thread : threads){
         thread.join();
     }
 
-    std::cout << counter.value << std::endl;	
+    std::cout << counter.value << std::endl;
+
+    return 0;
 }
 
+
+/*
+Output with multiple runs
+
+ashish@ubuntu:~/projects/CPPThread$ rm a.out ;g++ synchro_001.cpp  -std=c++11 -pthread ; ./a.out
+25000
+ashish@ubuntu:~/projects/CPPThread$ rm a.out ;g++ synchro_001.cpp  -std=c++11 -pthread ; ./a.out
+24872
+ashish@ubuntu:~/projects/CPPThread$ rm a.out ;g++ synchro_001.cpp  -std=c++11 -pthread ; ./a.out
+24912
+ashish@ubuntu:~/projects/CPPThread$ rm a.out ;g++ synchro_001.cpp  -std=c++11 -pthread ; ./a.out
+24900
+ashish@ubuntu:~/projects/CPPThread$ rm a.out ;g++ synchro_001.cpp  -std=c++11 -pthread ; ./a.out
+24833
+ashish@ubuntu:~/projects/CPPThread$
+
+*/
