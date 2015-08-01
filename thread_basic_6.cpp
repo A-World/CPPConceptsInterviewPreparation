e3553d8b2ce7c1d66ceaf8433eefb21a0e4b82c9
ashish <ashishrmodi16@gmail.com>
2015-08-01 04:58:43 -0700

    Start a thread with a lambda

--- thread_basic_6.cpp
+++ thread_basic_6.cpp
@@ -1,17 +1,24 @@
+// Refer http://baptiste-wicht.com/posts/2012/03/cpp11-concurrency-part1-start-threads.html
+
 #include <thread>
 #include <iostream>
 #include <vector>
 
-int main(){
+int main()
+{
     std::vector<std::thread> threads;
 
-    for(int i = 0; i < 5; ++i){
-        threads.push_back(std::thread([](){
-            std::cout << "Hello from thread " << std::this_thread::get_id() << std::endl;
-        }));
+    for(int i = 0; i < 5; ++i)
+	{
+        threads.push_back(std::thread([]()
+			{
+				std::cout << "Hello from thread " << std::this_thread::get_id() << std::endl;
+			}
+		));
     }
 
-    for(auto& thread : threads){
+    for(auto& thread : threads)
+	{
         thread.join();
     }
 
