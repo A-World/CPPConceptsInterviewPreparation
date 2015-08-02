e6f0ab98e2953e55972979aae066f45b7bda38b8
ashish <ashishrmodi16@gmail.com>
2015-08-02 16:07:12 -0700

    Added part implementation for the message queue

--- mutex_eg6.cpp
+++ mutex_eg6.cpp
@@ -3,9 +3,9 @@
 #include <cerrno>
 #include <pthread.h>
 #include <iostream>
+#include <queue>
 
-
-
+// Forward Declaration
 class MutexHelper;
 
 class Mutex
@@ -96,14 +96,12 @@ bool Mutex::Create()
     }
 
     m_created = true;
-
     return true;
 }
 
 void Mutex::Destroy()
 {
     assert(true == m_created);
-
     errno = 0;
     if(pthread_mutex_destroy(&m_mutexHandle))
     {
@@ -115,7 +113,6 @@ void Mutex::Destroy()
 void Mutex::Lock()
 {
     assert(true == m_created);
-
     errno = 0;
     if(pthread_mutex_lock(&m_mutexHandle))
     {
@@ -125,7 +122,6 @@ void Mutex::Lock()
 bool Mutex::Unlock()
 {
     assert(true == m_created);
-
     errno = 0;
     if(pthread_mutex_unlock(&m_mutexHandle))
     {
@@ -137,7 +133,6 @@ bool Mutex::Unlock()
 bool Mutex::Try()
 {
     assert(true == m_created);
-
     errno = 0;
     if(0 == pthread_mutex_trylock(&m_mutexHandle))
     {
@@ -153,6 +148,54 @@ bool Mutex::Try()
     return false;
 }
 
+template <class _ty>
+class MessageQueue
+{
+    private : 
+        bool m_created;
+        std::queue <_ty> m_queue;
+        Mutex m_mutex;
+        pthread_cond_t m_cond;
+
+    public:
+        MessageQueue() : m_created(false) {}
+        ~MessageQueue() { assert(false == m_created);}
+
+        bool Create()
+        {
+            assert(false == m_created);
+            if(false == m_mutex.Create())
+            {
+                return false;
+            }
+            if(pthread_cond_init(&m_cond,0))
+            {
+                m_mutex.Destroy();
+                return false;
+            }
+            m_created = true;
+            return true;        
+        }
+        
+        void Destroy()
+        {
+            assert(true == m_created);
+
+            m_mutex.Lock();
+            if(pthread_cond_destroy(&m_cond))
+            {
+                // Error in destroying cond variable
+            }
+            m_mutex.Unlock();
+            m_mutex.Destroy();
+        }
+
+        bool Send (const _ty &val)
+        {
+            assert (true == m_created);
+            return true;
+        }
+};
 
 
 int main()
