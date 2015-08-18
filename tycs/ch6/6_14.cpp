5de508cb8f8c334a9f0a37eccb06a71cff0a7737
ashish <ashishrmodi16@gmail.com>
2015-08-17 22:14:20 -0700

    TODO : fix the infinite loop issue

--- tycs/ch6/6_14.cpp
+++ tycs/ch6/6_14.cpp
@@ -1,6 +1,85 @@
+/*
+		This program runs into infinite loop, please fix it.
+*/
+
 #include <iostream>
 using namespace std;
 
+class time24;
+
+class time12
+{
+	int hour;
+	int minute;
+	int second;
+	public :
+	time12 (int h, int m, int s);
+	operator time24();
+};
+class time24
+{
+	int hour;
+	int minute;
+	int second;
+	public :
+	time24(int h, int m, int s);
+	operator time12();
+};
+
+time12 :: time12(int h, int m, int s)
+{
+	hour = h; minute = m;second = s;
+}
+
+time12 :: operator time24()
+{
+	time12 t (hour,minute,second);
+	return t;
+}
+
+time24 ::time24 (int h, int m, int s)
+{
+	hour = h; minute = m ; second = s;
+}
+time24 :: operator time12()
+{
+	time12 t (hour, minute,second);
+	return t;
+}
+
+int main()
+{
+	time12 a(11,23,45);
+	time24 b(10,35,23);
+	
+	a = b;
+	b = a;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef THIS_PROGRM_IS_IN_INFINITE_LOOP
 class time12;
 class time24;
 
@@ -36,12 +115,14 @@ class time24
 
 time12 :: operator time24()
 {
-	
+	time12 t;
+	return t;
 }
 
 time24 :: operator time12()
 {
-
+	time24 t;
+	return t;
 }
 
 int main()
@@ -52,10 +133,11 @@ int main()
 	time24 t24 ;
 	time12 t12_1 () ;
 	time24 t24_1 () ;
-	
+	t12 = t24;
+	t24 = t12;
 }
 
-
+#endif
 
 
 
