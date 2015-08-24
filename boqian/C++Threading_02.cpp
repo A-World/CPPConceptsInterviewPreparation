b53a1a9066a79e63a5982d568002c1430b6633bc
ashish <ashishrmodi16@gmail.com>
2015-08-24 08:48:19 -0700

    Handling thred exception

--- boqian/C++Threading_02.cpp
+++ boqian/C++Threading_02.cpp
@@ -1,5 +1,16 @@
 /*
     Thread Management
+
+    - Another approach, use a wrapper class, write the thread1.join inside destructor, 
+      whenever thread object goes out of scope, it shall call destructor, and t.join() function. 
+      This approach uses RAII concept.
+    
+    - Thread object can be constructed not only with regular function like function_1, 
+      but also with any callable object, such as functor or labmda function.
+    
+    
+    Q 1. : if i write t1.join multiple times, what is behaviour
+    
 */
 
 
@@ -15,20 +26,21 @@ void function_1()
 
 int main()
 {
-    std::thread t1 (function_1);    // At this point t1 starts running
-    // t1.join();       // main thread waits for t1 to finish.
-    t1.detach();        // t1 runs freely on its own, t1 become daemon process 
-                        // C++ run time library will be responsible for reclaim the resources used by t1 daemon.
+    std::thread t1 (function_1);    
     
-    if(t1.joinable())
+    try
+    {
+        for (int i = 0 ; i < 100; i++)
+            cout << "from main: "  << i << endl;
+    }
+    catch(...)
+    {        
         t1.join();
+        cout << "Execption caught"<<endl;
+        throw;
+    }
+    
+    cout << "executed main function t1.join()"<<endl;
+    t1.join();
     return 0;
 } 
-
-
-
-/*
-    Main thread run so fast that its execution is completed before the t1 execution, hence no message is printed out.
-
-
-*/
