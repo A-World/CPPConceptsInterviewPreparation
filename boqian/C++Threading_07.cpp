efe3f52c6cd536bbfa2a0a9cef4414f71017d9c1
ashish <ashishrmodi16@gmail.com>
2015-08-24 09:49:36 -0700

    Thread : ID function

---
+++ boqian/C++Threading_07.cpp
@@ -0,0 +1,60 @@
+/*
+    Thread Management : Associating thread with functor along with paramters, move function
+
+    - Another approach, use a wrapper class, write the thread1.join inside destructor,
+      whenever thread object goes out of scope, it shall call destructor, and t.join() function.
+      This approach uses RAII concept.
+
+    - Thread object can be constructed not only with regular function like function_1,
+      but also with any callable object, such as functor or labmda function.
+
+    - To pass string s as paramters to thread creation, pass it as additional paramters.
+
+    - A paramter to thread is always pass by value.
+
+    - In following example, child thread and parent thread sharing same memeory, which is string s.
+
+    - std::move shall move the string s from main thread to child thread.
+
+    - A thread can not be copied, to change the ownership of thread from one thread to another, use std::move(t1).
+
+	- To know, which thread is executing the current block of code, use following
+        std:: this_thread::get_id() //function
+
+    Q 1. : if i write t1.join multiple times, what is behaviour
+
+*/
+
+
+#include <iostream>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+class Fctor
+{
+    public :
+        void operator () (string msg)
+        {
+            cout << "T1 says " <<msg<<endl;
+            msg = "Trust is the mother of deceit";
+        }
+};
+
+int main()
+{
+    string s = "where there is no trust, there is no love";
+    cout << "Main thread id is " << std::this_thread::get_id()<<endl;
+
+    std::thread t1 ((Fctor()), std::move(s));   // After move(s), the s becomes empty.
+    cout <<"T1 id is " <<t1.get_id()<<endl;
+    std::thread t2 = std::move(t1);     // After execution of this, t1 becomes empty.
+    cout <<"T1 id is " <<t1.get_id()<<endl;
+    cout <<"T2 id is " <<t2.get_id()<<endl;
+
+    cout << "from main: "  << s << endl;
+    t2.join();
+
+    return 0;
+}
