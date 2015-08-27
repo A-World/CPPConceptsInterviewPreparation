15686e42924a9bbbbf3676181e4393b972bfe3fc
ashish <ashishrmodi16@gmail.com>
2015-08-27 10:41:41 -0700

    C++ : koening Lookup does not apply to class, it applies to namespace only

--- boqian/advancedC++/advanced_013.cpp
+++ boqian/advancedC++/advanced_013.cpp
@@ -1,5 +1,6 @@
 /*
     Introduction to C++	: Koening Lookup
+    // This is applicable to namespace and not to class
 */
 
 #include <iostream>
@@ -22,14 +23,32 @@ void g( A::X)
 }    
 */
 
+
+class C 
+{
+    public :
+        struct Y {};
+        static void h(Y y ) 
+        {
+            cout << "Calling C::h(Y) function "<<endl;
+        }
+};
+
 int main()
 {
     cout << "Hello World" <<endl;
     
+    // With namespace
     A::X x;
     A::g(x);        // This runs perfect as expected.
     g(x);           // function g will also be searched where its paramter types are defined.
                     // This is called Koening Lookup or Arguement dependent lookup (ADL)
+    
+    // With Class
+    C::Y y ;
+    C::h(y);        // Works fine as expected.
+    // h(y);           // Illegeal, Koening Lookup applies to namespaces only.
+    
     
     return 0;
 }
