a6fe73616c2f43316e1e400c99c56da5d5c83111
ashish <ashishrmodi16@gmail.com>
2015-09-04 11:19:36 -0700

    C++ : Public inheritace, A derived class should be able to do everything the base class can do. Illustrated

---
+++ boqian/advancedC++/advanced_036.cpp
@@ -0,0 +1,26 @@
+/*
+    Introduction to C++	:
+    Video       :   Advanced C++ Maintain is-a Relation for Public Inheritance
+    Program  Part 3 : Public, protected and private inheritance - introduction
+
+    - public inheritance : IS A relationship.
+    - A derived class should be able to do everything the base class can do.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class bird { };
+class flyable : public bird {  public :  void fly()    { }  };
+class penguin : public bird {};
+
+//          Design Principle
+//    A derived class should be able to do everything the base class can do.
+//    - In this example we achieved our design principle by creating new class for flyable birds.
+
+int main()
+{
+    return 0;
+}
