55704d2f603d8078d691bd33c14a43914ef4eac1
ashish <ashishrmodi16@gmail.com>
2015-09-04 11:58:35 -0700

    C++ : Public Inheritance : Inheritance Interface + Implementation. Illustrated

--- boqian/advancedC++/advanced_041.cpp
+++ boqian/advancedC++/advanced_041.cpp
@@ -1,7 +1,7 @@
 /*
     Introduction to C++	:
     Video           :   Advanced C++ Duality Of Public Inheritance - Interface & Implementation
-    Program  Part 1 :   Public Inheritance : Inheritance Interface + Implementation.
+    Program  Part 1 :   Public Inheritance : Implementation without interface
 */
 
 #include <iostream>
@@ -16,12 +16,17 @@ class dog
     virtual void bark () = 0;
     void run () { cout << "I am running "<<endl;}   // This is non virtual method, so it shall not be overriden,
     // This method also contains implemenation.
+    virtual void eat () { cout << "I am eating" <<endl; }
+    protected : 
+        void sleep() { cout << "I am sleeping" <<endl; }  // Only implementaion is provided, no direct interface is provided.
 };
 
 class yellowdog : public dog
 {
 public :
     virtual void bark() { cout << "yellowdog :: I am yellow dog "<<endl;   }
+    void isleep() { sleep(); }
+    
 };
 
 int main()
