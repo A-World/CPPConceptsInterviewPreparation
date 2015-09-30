e91d05c6a7e901101e8172273fe56ca08906b284
ashish <ashishrmodi16@gmail.com>
2015-09-30 10:00:21 -0700

    Added process related files

---
+++ process/ProcessDetails.cpp
@@ -0,0 +1,38 @@
+// Following are the C methods that I need to go through
+Important API's for linux programmers. 
+1. fork()
+2. clone()
+3. pipe(), pipe2()
+4. wait(),waitpid(),waitid()
+5. sched() : A set of scheduling related APIs, nearly 12 APIs are present.
+6. proc()
+7. vfork()
+8. unshare()
+9. read()
+10. write()
+11. popen()
+12. signal()
+13. select()
+14. accept()
+15. connect()
+16. poll()
+17. recv()
+18. send()
+19. bind()
+20. listen()
+21. socket()
+22. unix()
+          
+    
+// Following are the types of inter process communiction in linux
+    1.  Signals
+    2.  Pipes
+    3.  FIFOs
+    4.  Message Queues
+    5.  Semaphores
+    6.  Shared Memory.
+
+
+
+
+
